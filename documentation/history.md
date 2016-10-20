460c28c0584043fa86081cc4154f3e7d716b5ec2
Tommmi <thomas@gerlach-software.de>
2016-10-20 20:39:18 +0200

    no important changes - new version of TUtils.Common.dll

--- documentation/history.md
+++ documentation/history.md
@@ -1,4 +1,5 @@
 # History of Resx2Js
-
+## Version 1.0.0.0 - 20.10.2016
+no important changes - new version of TUtils.Common.dll
 ## Version 1.0.0.0 - 11.07.2016
 Initial version
--- source/Resx2Js/postbuild.bat
+++ source/Resx2Js/postbuild.bat
@@ -1,4 +1,4 @@
-﻿::
+::
 
 :: %1: project dir
 :: %2: debug release
@@ -9,4 +9,4 @@
 ::**********************************************************
 del "%1..\..\lib\*.*" /f /q /s
 copy "%1bin\%2\%3" "%1..\..\lib\%3"
-copy "%1..\external\TUtils.Common\*.*" "%1..\..\lib\*.*"
+copy "%1..\external\TUtils.Common\TUtils.Common.dll" "%1..\..\lib\TUtils.Common.dll"
